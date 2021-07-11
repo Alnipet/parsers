716dd4cae70fd3719d54106b64a80061dca974fd
Alnipet <a777petrov@gmail.com>
2021-07-12 00:08:42 +0300

    save in csv

--- Lunda Parser/Parser.py
+++ Lunda Parser/Parser.py
@@ -18,6 +18,9 @@ ParserResult = collections.namedtuple(
     )
 )
 
+HEADERS = (
+)
+
 class Client:
 
     def __init__(self):
@@ -25,6 +28,9 @@ class Client:
         self.session.headers = {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36',
             'accept': '*/*'}
+        self.chars_title_result = []
+        self.chars_result = []
+
 
     def load_page(self):
         url = 'https://lunda.ru/catalog/category/c6002.html'
@@ -75,47 +81,59 @@ class Client:
             self.pars_product_items(product_item=product_item)
 
     def pars_product_items(self, product_item):
-        product_chars_titles = product_item.select('th.products-table__header')
+        product_chars_titles = product_item.select('span.products-table__header-text')
         product_chars = product_item.select('td.products-table__item')
-        #print(product_chars_titles)
         titles = []
         chars = []
+        if not product_chars_titles:
+            logger.error('no chars title')
         if not product_chars:
             logger.error('no chars')
         for product_chars_title in product_chars_titles:
             title = self.char_title(product_chars_title=product_chars_title)
+            title = " ".join(title.split())
             chars_title = title
             titles.append(chars_title)
-            print(titles)
         for product_char in product_chars:
-            # print(product_char)
             name = self.char_name(product_char=product_char)
-            #title = self.char_title(product_char=product_char)
+            name = " ".join(name.split())
             char = name
             chars.append(char)
-            #print(name)
-        print(titles)
-        print(chars)
+        if titles != []:
+            self.chars_result.append(titles)
+
+        if chars != []:
+            self.chars_result.append(chars)
+        #self.chars_title_result.append(titles)
+
+        #print(self.chars_title_result)
+        #print(self.chars_result)
 
         return chars
 
     def char_title(self, product_chars_title):
         title = product_chars_title.get_text()
-        return title
+        return title.strip()
 
     def char_name(self, product_char):
         name = product_char.get_text()
-        r = name.strip()
-        return r #title, name.strip()
+        return name.strip()
 
 
     def save_results(self):
         path = 'D:/PythonProg/Parsers/Lunda Parser/test.csv'
-        with open(path, 'w') as f:
-            writer = csv.writer(f)  # , quoting=csv.QUOTE_MINIMAL)
-            writer.writerow()
-            # for item in self.result:
-            # pass
+        with open(path, 'w', newline='') as f:
+            writer = csv.writer(f , delimiter=';')
+            x = 0
+            for item in self.chars_result:
+                if item:
+                    print(item)
+                    writer.writerow(item)
+                    x += 1
+                else:
+                    pass
+            print(x)
+
 
     def run(self):
         text = self.load_page()
@@ -126,3 +144,4 @@ class Client:
 if __name__ == '__main__':
     parser = Client()
     parser.run()
+
